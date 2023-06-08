b8ba4654d1373958acc6906bff0e18218f1db48d
Josue Teoabaldo dos Santos <josueteobaldo220@hotmail.com>
2023-06-08 20:05:24 -0300

    commitado kk

--- challenge_schneider/src/components/LoginPageComponents/LoginTitle/index.jsx
+++ challenge_schneider/src/components/LoginPageComponents/LoginTitle/index.jsx
@@ -1,25 +1,11 @@
 import React from "react";
-import { useSpring, animated } from "@react-spring/web";
 import styles from "./LoginTitle.module.css";
 
 export default function LoginTitle() {
-  const fadeIn = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: { duration: 1500 },
-  });
-
-  const slideIn = useSpring({
-    from: { transform: "translateX(-100px)", opacity: 0 },
-    to: { transform: "translateX(0)", opacity: 1 },
-    config: { duration: 1000 },
-  });
 
   return (
-    <animated.div style={fadeIn}>
       <p className={styles.title}>
-        <span>Schneider</span>EcoConnect
+        <span className={styles.fadeIn}>Schneider</span>EcoConnect
       </p>
-    </animated.div>
   );
 }
